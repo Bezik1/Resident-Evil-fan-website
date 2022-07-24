2aa4f27d1368ef1ae36c829b86c2edcee2186bd3
Bezik1 <mateusz.adamowicz.lo1@gmail.com>
2022-07-24 21:54:42 +0200

    useResource update
    Update switch in useResource hooks to better running

--- src/hooks/useResource.tsx
+++ src/hooks/useResource.tsx
@@ -47,14 +47,14 @@ export const useResource = (type : RESOURCE_TYPES) =>{
     })
 
     useEffect(() =>{
-        switch(true){
-            case type === flower: 
+        switch(type){
+            case flower: 
                 setData({
                         url: FLOWER_URL, 
                         header: 'African Laboratory', 
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure'})
                     break
-                case type === institution_1:
+                case institution_1:
                     setData({
                         url: RACOON_URL, 
                         header: 'Racoon City', 
@@ -63,13 +63,13 @@ export const useResource = (type : RESOURCE_TYPES) =>{
                         color: new Color('#3c80b5')
                     })   
                     break
-                case type === arklay:
+                case arklay:
                     setData({
                         url: ARKLAY_URL, 
                         header: 'Arklay Laboratory', 
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure'})
                     break
-                case type === europe_lab:
+                case europe_lab:
                     setData({
                         url: EUROPE_URL,
                         header: 'Europe Laboratory', 
@@ -78,42 +78,42 @@ export const useResource = (type : RESOURCE_TYPES) =>{
                         color: new Color('#3c80b5')
                     })
                      break
-                case type === info:
+                case info:
                     setData({
                         url: LOGO_URL,
                         header: 'Umbrella Corporation',
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure',
                     })
                     break
-                case type === grenland:
+                case grenland:
                     setData({
                         url: GRENLAND_URL,
                         header: 'Ashfords Laboratory',
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure',
                     })
                     break
-                case type === spain:
+                case spain:
                     setData({
                         url: LAS_PLAGAS,
                         header: 'Las Plagas Village',
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure',
                     })
                     break
-                case type === village:
+                case village:
                     setData({
                         url: VILLAGE_URL,
                         header: 'Megamycete Village',
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure',
                     })
                     break
-                case type === kijuju:
+                case kijuju:
                     setData({
                         url: KIJUJU_URL,
                         header: 'Kijuju incident',
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure',
                     })
                     break
-                case type === shanghai:
+                case shanghai:
                     setData({
                         url: SHANGHAI_URL,
                         header: 'Shanghai Incident',
