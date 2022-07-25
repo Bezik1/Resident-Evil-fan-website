3bd7c019ac41771787c6f74fe485a62851b94952
Bezik1 <mateusz.adamowicz.lo1@gmail.com>
2022-07-25 12:04:36 +0200

    Optimization of useResource hook
    Repair dependency array in useEffect hook inside useResource to better working in project

--- src/hooks/useResource.tsx
+++ src/hooks/useResource.tsx
@@ -26,9 +26,6 @@ export const useResource = (type : RESOURCE_TYPES) =>{
         resource: '', 
         obj: null!
     })
-
-    const { flower, institution_1, arklay, europe_lab, 
-        info, grenland, spain, village, kijuju, shanghai } = RESOURCE_TYPES
     const { url, header, resource, obj, color } = data
     
     const { NEMESIS_URL, DOOR_URL } = MODELS_URLS
@@ -47,6 +44,11 @@ export const useResource = (type : RESOURCE_TYPES) =>{
     })
 
     useEffect(() =>{
+        const BLUE_COLOR = new Color('#3c80b5')
+
+        const { flower, institution_1, arklay, europe_lab, 
+            info, grenland, spain, village, kijuju, shanghai } = RESOURCE_TYPES
+
         switch(type){
             case flower: 
                 setData({
@@ -60,7 +62,7 @@ export const useResource = (type : RESOURCE_TYPES) =>{
                         header: 'Racoon City', 
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure',
                         obj: <primitive object={DOOR_OBJ} position={[0, 0, -30]} />,
-                        color: new Color('#3c80b5')
+                        color: BLUE_COLOR
                     })   
                     break
                 case arklay:
@@ -75,7 +77,7 @@ export const useResource = (type : RESOURCE_TYPES) =>{
                         header: 'Europe Laboratory', 
                         resource: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure',
                         obj: <primitive object={OBJ} position={[0, 1.5, 1]} />,
-                        color: new Color('#3c80b5')
+                        color: BLUE_COLOR
                     })
                      break
                 case info:
@@ -121,7 +123,7 @@ export const useResource = (type : RESOURCE_TYPES) =>{
                     })
                     break
         }
-    }, [type, flower, institution_1, arklay, europe_lab, OBJ, info, grenland, spain, village, kijuju, shanghai, DOOR_OBJ])
+    }, [type, OBJ, DOOR_OBJ])
 
     return { url, header, resource, obj, color }
 }
